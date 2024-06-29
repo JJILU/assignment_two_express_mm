dc4f23d1dce14efab237ab0a25852fd9f5c1fe3c
Naomi Kapele <naomikapele@booknowzambia.com>
2024-06-29 06:01:52 +0300

    add response to route and image for assignment

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const app = express();
 
 // middleware
 app.use('/users', (req,res,next) => {
+  console.log('middleware one');
+  res.send({message: "welcome to the nodejs course"});
+});
 
+app.use('/', (req,res,next) => {
+  console.log('middleware one');
 });
